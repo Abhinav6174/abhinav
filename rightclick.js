1c272939c35f65bf2a2f326c6a134ee3189eeb75
ABHINAV <68990598+Abhinav6174@users.noreply.github.com>
2021-09-11 20:23:36 +0530

    Update rightclick.js

--- rightclick.js
+++ rightclick.js
@@ -1,3 +1,13 @@
 document.addEventListener("contextmenu", function(e) {
     e.preventDefault();
 }, false);
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
